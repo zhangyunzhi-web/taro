fd35763aaa72dc9b625ca7f8c22a31ebe4248aa6
张云志 <zhangyunzhi@hello-orange.com>
2020-09-17 17:29:42 +0800

    Initial commit

---
+++ src/pages/index/index.vue
@@ -0,0 +1,18 @@
+<template>
+  <view class="index">
+    <text>{{ msg }}</text>
+    <text>??????</text>
+  </view>
+</template>
+
+<script>
+import './index.scss'
+
+export default {
+  data() {
+    return {
+      msg: 'Hello world!'
+    }
+  }
+}
+</script>
